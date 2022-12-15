8597214af65f3c51872610d24bbeb440dd4c7d8e
Javier Morales de Vera <javierMorales9@gmail.com>
2022-12-15 18:07:24 +0100

    Fix linter problems

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ module.exports = {
     cfg.module.rules.push({
       test: /\.md$/,
       loader: 'frontmatter-markdown-loader',
-      options: { mode: ['react-component'] }
-    })
+      options: { mode: ['react-component'] },
+    });
     return cfg;
-  }
-}
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
-import Head from 'next/head';
 import { Component } from 'react';
+// @ts-ignore
 import { attributes, react as HomeContent } from '../content/home.md';
+import Script from 'next/script';
 
 export default class Home extends Component {
   render() {
     let { title, cats } = attributes;
     return (
       <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
         <article>
+          <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
           <h1>{title}</h1>
           <HomeContent />
           <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
+            {cats.map(
+              (cat: { name: string; description: string }, k: number) => (
+                <li key={k}>
+                  <h2>{cat.name}</h2>
+                  <p>{cat.description}</p>
+                </li>
+              ),
+            )}
           </ul>
         </article>
       </>
